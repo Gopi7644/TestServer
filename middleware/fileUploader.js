d5a1f79ce75a97f21895cb03bfcb714821c65a87
Gopi Kumar <gopikumar764482@gmail.com>
2025-03-24 23:06:46 +0530

    fileuploader push

---
+++ middleware/fileUploader.js
@@ -0,0 +1,31 @@
+import multer from "multer";
+import { CloudinaryStorage } from "multer-storage-cloudinary";
+import { v2 as cloudinary } from "cloudinary";
+import dotenv from "dotenv";
+dotenv.config();
+// import { v2 as cloudinary } from "cloudinary";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+  secure: true,
+});
+
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: "profileImage",
+    format: async (req, file) => "png",
+    
+    public_id: (req, file) => {
+      if (!file || !file.originalname) {
+        throw new Error("Invalid file upload");
+      }
+      return file.originalname.split(".")[0] + "-" + Date.now(); // Ensure unique filename
+    },
+  },
+});
+
+export const upload = multer({ storage: storage });
+export default cloudinary;
