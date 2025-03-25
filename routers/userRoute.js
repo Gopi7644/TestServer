a5ff6bf319c0d8da25cba6990224b955871b8ad8
Gopi Kumar <gopikumar764482@gmail.com>
2025-03-25 23:17:51 +0530

    add video uploader

--- routers/userRoute.js
+++ routers/userRoute.js
@@ -1,8 +1,9 @@
 import express from "express";
 import { upload } from "../middleware/fileUploader.js";
-import { UserTwo } from "../model/user.model.js";
+import { UserTwo, Video } from "../model/user.model.js";
 
 import { create, deleteuser, getAll, getById, update } from "../controller/userController.js";
+import { uploadVideo } from "../middleware/videoUploader.js";
 
 
 export const route = express.Router();
@@ -30,6 +31,34 @@ route.post("/upload", upload.single("image"), async (req, res) => {
       res.status(500).json({ error: error.message });
     }
   });
+
+
+  // Video Upload API
+route.post("/upload-video", uploadVideo.single("video"), async (req, res) => {
+  try {
+      if (!req.file) {
+          return res.status(400).json({ message: "No video uploaded" });
+      }
+
+      console.log("Uploaded Video:", req.file);
+
+      // नया Video डेटा MongoDB में Save करें
+      const newVideo = new Video({
+          title: req.body.title,
+          description: req.body.description,
+          videoUrl: req.file.path // ✅ Cloudinary से आया Video URL
+      });
+
+      await newVideo.save();
+
+      res.status(201).json({
+          message: "Video uploaded successfully",
+          video: newVideo
+      });
+  } catch (error) {
+      res.status(500).json({ error: error.message });
+  }
+});
 
 route.get("/getAll", getAll);
 
